bedb1f86173b8c906d1b86fee613bb9d670b64a5
Benson-Ochieng <markbenson10368@gmail.com>
2022-07-27 12:43:59 +0300

    humbergar menu full page

--- js/script.js
+++ js/script.js
@@ -2,4 +2,5 @@ var mobileNav = document.querySelector('.mobile_nav')
 var bars = document.querySelector('.bars')
 bars.onclick = function(){
    mobileNav.classList.toggle('show_nav')
+   document.body.classList.toggle('overflow_is_hidden')
 }
